c42aeeb2fa71189286aed79f7a4f2ea51ffa3fe2
Abdallah Abis <Abis.Abdallah@gmail.com>
2020-03-21 23:49:38 +0100

    Adding a simple Shop page to see if the data is appearing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.component";
+import ShopPage from "./pages/Shop/Shop.component";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Shop/Shop.component.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import SHOP_DATA from "./Shop.data";
+import CollectionPreview from "../../components/Collection-preview/Collection-preview.component";
+
+class ShopPage extends Component {
+  state = {
+    collections: SHOP_DATA
+  };
+  render() {
+    const { collections } = this.state;
+    console.log(collections);
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
