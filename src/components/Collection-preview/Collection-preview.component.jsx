8d21cfe2c58fbd4d15fa8c0f459115f939e8afdb
Abdallah Abis <Abis.Abdallah@gmail.com>
2020-03-22 16:12:57 +0100

    Collection-item Added, and Done styles for the Shop page

--- src/components/Collection-preview/Collection-preview.component.jsx
+++ src/components/Collection-preview/Collection-preview.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-// import CollectionItem from "../Collection-item/Collection-item.component";
+import CollectionItem from "../Collection-item/Collection-item.component";
 
 import "./Collection-preview.styles.scss";
 
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>
