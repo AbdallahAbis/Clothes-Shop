a08618d13194dd432fc01d082bb04d695dd13403
Abdallah Abis <Abis.Abdallah@gmail.com>
2020-03-23 22:34:48 +0100

    sign in with email and password finished.

--- src/components/Sign-in/Sign-in.component.jsx
+++ src/components/Sign-in/Sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import FormInput from "../Form-input/Form-input.component";
 import CustomButton from "../Custom-button/Custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./Sign-in.styles.scss";
 
@@ -12,8 +12,16 @@ class SignIn extends Component {
     password: ""
   };
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
 
     this.setState({ email: "", password: "" });
   };
