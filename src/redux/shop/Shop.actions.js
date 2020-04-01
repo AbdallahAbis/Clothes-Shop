ac6b74d1f7d7a732f97bdc050fd21a426bc58b24
Abdallah Abis <Abis.Abdallah@gmail.com>
2020-04-01 16:18:50 +0100

    clearing the cart after a successful sign out implemented

--- src/redux/shop/Shop.actions.js
+++ src/redux/shop/Shop.actions.js
@@ -1,10 +1,5 @@
 import ShopActionTypes from "./shop.types";
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap
-} from "../../firebase/firebase.utils";
-
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
@@ -17,19 +12,21 @@ export const fetchCollectionFailure = errorMessage => ({
   payload: errorMessage
 });
 
-export const fetchCollectionsStartAsync = () => {
-  return dispatch => {
-    const collectionRef = firestore.collection("collections");
-    dispatch(fetchCollectionsStart());
 
-    collectionRef
-      .get()
-      .then(snapshot => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionSuccess(collectionsMap));
-      })
-      .catch(error => {
-        dispatch(fetchCollectionFailure(error.message));
-      });
-  };
-};
+
+// export const fetchCollectionsStartAsync = () => {
+//   return dispatch => {
+//     const collectionRef = firestore.collection("collections");
+//     dispatch(fetchCollectionsStart());
+
+//     collectionRef
+//       .get()
+//       .then(snapshot => {
+//         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+//         dispatch(fetchCollectionSuccess(collectionsMap));
+//       })
+//       .catch(error => {
+//         dispatch(fetchCollectionFailure(error.message));
+//       });
+//   };
+// };
